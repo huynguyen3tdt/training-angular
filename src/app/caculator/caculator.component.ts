6c750cee03b3605295c2c86bbbc09f927eb79a3a
huynguyen3tdt <huynguyen3@cqtdt.asia>
2022-12-22 08:34:22 +0700

    caculator

---
+++ src/app/caculator/caculator.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-caculator',
+  templateUrl: './caculator.component.html',
+  styleUrls: ['./caculator.component.css']
+})
+export class CaculatorComponent {
+  text = "duy huy";
+  result: string = "";
+}
